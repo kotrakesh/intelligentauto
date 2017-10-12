483295d75ccf937f0ef7f0725e420621712babd7
kotrakesh <kotrakesh@gmail.com>
2017-10-12 11:13:27 +0200

    workflow

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hellworld():
+def login():
 	return render_template('%s.html' % 'login')
-
+@app.route('/workflow')
+def workflow():
+	return render_template('%s.html' % 'index')#
+@app.route('/workflow/recruiting-applet')
+def workflowdetail():
+	return render_template('%s.html' % 'workflow-detail')	
 @app.route('/dashboard')
-def hello_world():
-	return render_template('%s.html' % 'index')
+def dashbord():
+	return render_template('%s.html' % 'dashboard')
 if __name__ == '__main__':
     app.run(debug=True)    
